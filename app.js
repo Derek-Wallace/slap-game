b06326a3783439e2fdb3dc07b14db7daffedba62
Derek-Wallace <lacrosseid@gmail.com>
2021-05-13 17:00:25 -0600

    defense

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 var health = 100
 var hits = 0
+var stance = ''
 draw()
-
-
 
 function draw() {
   let template = `${health}`
@@ -22,34 +21,81 @@ function slap() {
 
 }
 
+function guard(){
+  stance = 'guarded'
+  document.getElementById("dummyImg").src="https://srk.shib.live/images/c/cc/%28ryulparry%29.gif"
+  setTimeout(resetImg, 500)
+}
+
 function punch() {
-  health -= 5
+  if (stance == 'guarded'){
+    health -= 2
+    draw()
+    document.getElementById("dummyImg").src="https://srk.shib.live/images/c/cc/%28ryulparry%29.gif"
+    document.getElementById("img").src="https://srk.shib.live/images/f/f4/%28kenmp%29.gif"
+    setTimeout(resetImg, 600)
+    stance = ''
+  }
+  else {health -= 5
   draw()
   document.getElementById("img").src="https://srk.shib.live/images/f/f4/%28kenmp%29.gif"
   document.getElementById("dummyImg").src="https://srk.shib.live/images/4/4d/%28ryuw%29.gif"
   setTimeout(resetImg, 600)
+  stance = '' }
   if (health <= 0) {
     setTimeout(endGame, 100)
   }
 }
 
+function duck(){
+  stance = 'ducking'
+  document.getElementById("dummyImg").src="https://srk.shib.live/images/9/9b/%28ryucmp%29.gif"
+  setTimeout(resetImg, 500)
+}
+
 function kick() {
-  health -= 3
+  if (stance == 'ducking'){
+    health -= 1
+    draw()
+    document.getElementById("dummyImg").src="https://srk.shib.live/images/9/9b/%28ryucmp%29.gif"
+    document.getElementById("img").src="https://srk.shib.live/images/d/d1/%28kenmk%29.gif"
+    setTimeout(resetImg, 600)
+    stance = ''
+  }
+  else {health -= 3
   draw()
   document.getElementById("img").src="https://srk.shib.live/images/d/d1/%28kenmk%29.gif"
   document.getElementById("dummyImg").src="https://srk.shib.live/images/4/4d/%28ryuw%29.gif"
   setTimeout(resetImg, 750)
+  stance = ''}
   if (health <= 0) {
     setTimeout(endGame, 100)
   }
 }
 
+function jump(){
+  stance = 'jumping'
+  document.getElementById("dummyImg").src="https://srk.shib.live/images/e/e1/%28ryujump%29.gif"
+  setTimeout(resetImg, 700)
+}
+
 function Hadouken() {
-  health -= 50
+  if (stance == 'jumping'){
+    health -= 0
+    draw()
+    yell()
+    document.getElementById("dummyImg").src="https://srk.shib.live/images/e/e1/%28ryujump%29.gif"
+    document.getElementById("img").src="https://srk.shib.live/images/2/2a/%28kenhdk%29.gif"
+    setTimeout(resetImg, 600)
+    stance = ''
+  }
+  else {health -= 50
   draw()
+  yell()
   document.getElementById("img").src="https://srk.shib.live/images/2/2a/%28kenhdk%29.gif"
   document.getElementById("dummyImg").src="https://srk.shib.live/images/4/4d/%28ryuw%29.gif"
   setTimeout(resetImg, 1000)
+  stance = ''}
   if (health <= 0) {
     setTimeout(endGame, 100)
   }
@@ -57,6 +103,7 @@ function Hadouken() {
 
 function reset() {
   health = 100
+  stance = ''
   draw()
 }
 
@@ -68,4 +115,10 @@ function endGame() {
 function resetImg(){
   document.getElementById("img").src="https://srk.shib.live/images/6/69/Ken3s-stance.gif"
   document.getElementById("dummyImg").src="https://srk.shib.live/images/2/20/Ryu3s-stance.gif"
+}
+
+function yell(){
+  const yellPlayer = document.getElementById('sound')
+  yellPlayer.setAttribute('src', 'https://www.myinstants.com/media/sounds/hadouken.mp3')
+  yellPlayer.play()
 }
